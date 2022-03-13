5a0addf0a0a427236a38f4c4a043793d4b0bb2f8
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-13 13:56:54 +0800

    init

--- App20220313FBLogin/AppDelegate.swift
+++ App20220313FBLogin/AppDelegate.swift
@@ -6,15 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
+    func application(_ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      FirebaseApp.configure()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+      return true
     }
 
     // MARK: UISceneSession Lifecycle
