4591bc2ba1df25efaef27bd35f48f5d5ba664002
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-26 00:34:15 +0800

    add FB login & logout event

--- App20220313FBLogin/ViewController.swift
+++ App20220313FBLogin/ViewController.swift
@@ -6,17 +6,32 @@
 //
 
 import UIKit
+import Firebase
 import FBSDKLoginKit
 
 class ViewController: UIViewController, LoginButtonDelegate {
     // FB Login OK
     func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
         print("Login FB")
+        
+        let credential = FacebookAuthProvider
+          .credential(withAccessToken: AccessToken.current!.tokenString)
+        
+        Auth.auth().signIn(with: credential) { authData, error in
+            print(error?.localizedDescription ?? "")
+            print("Complate")
+        }
     }
     
     // FB Logout OK
     func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
         print("Logout FB")
+        
+        do{
+            try Auth.auth().signOut()
+        }catch{
+            print(error.localizedDescription)
+        }
     }
     
     
