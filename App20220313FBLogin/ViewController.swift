af43e7a5d91d53a950a9939b8e3308b6d22b2cc7
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-26 00:06:13 +0800

    Login & Logout Event

--- App20220313FBLogin/ViewController.swift
+++ App20220313FBLogin/ViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
+    // FB Login OK
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        print("Login FB")
+    }
+    
+    // FB Logout OK
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("Logout FB")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let loginButton = FBLoginButton()
         loginButton.center = view.center
+        loginButton.delegate = self
         view.addSubview(loginButton)
     }
 
